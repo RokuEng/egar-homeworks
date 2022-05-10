91325fb0daed37ad8b10d705f442cfd6011bf3e0
Alexandr <alexandr.stenkin@gmail.com>
2022-05-10 23:09:22 +0300

    Удаление тестового кода

--- src/main/java/com/RokuEng/homeworks/les6/repos/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.RokuEng.homeworks.les6.repos;
-
-import com.RokuEng.homeworks.les6.domain.persistence.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepo extends JpaRepository<Person, Long> {
-}
