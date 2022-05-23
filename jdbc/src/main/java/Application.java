f9315669cbc0ea1e9535d8271becefef638db85c
Alexandr <alexandr.stenkin@gmail.com>
2022-05-23 10:11:05 +0300

    Чистка кода

--- jdbc/src/main/java/Application.java
+++ jdbc/src/main/java/Application.java
@@ -3,7 +3,7 @@ import exception.CoffeeNotFoundException;
 import exception.SQLConnectionException;
 
 public class Application {
-	public static void main(String[] args) throws SQLConnectionException {
+	public static void main(String[] args)  {
 		OrganisationDao dao = new OrganisationDao();
 		dao.delete(dao.findById(1).orElseThrow(CoffeeNotFoundException::new));
 		System.out.println(dao.findById(1).get());
